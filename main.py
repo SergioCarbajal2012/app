e239ecfdf19aae79b7200b2abc73cf3397d92c1f
SergioCarbajal2012 <sergio.mcarbajal2012@gmail.com>
2026-06-21 14:06:29 -0600

    Refactor Firebase initialization for Streamlit Cloud
    Update Firebase initialization to use Streamlit secrets for cloud deployment.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
+import json
 
-# Inicialización de Firebase
 if not firebase_admin._apps:
-    cred = credentials.Certificate('firebase_key.json') # Llave de servicio
+    # Si estamos en Streamlit Cloud, usamos los Secretos del sistema
+    if "firebase" in st.secrets:
+        firebase_details = dict(st.secrets["firebase"])
+        cred = credentials.Certificate(firebase_details)
+    # Si estamos en local, usamos el archivo JSON
+    else:
+        cred = credentials.Certificate('firebase_key.json')
+        
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
